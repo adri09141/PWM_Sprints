1a8dfd34241c76eedd2417c77ec45ce669127671
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-02-23 16:03:03 +0000

    Cree index.html y  xlu-include.js y cambie catalogoRecuadro y a

---
+++ xlu-include-file.js
@@ -0,0 +1,78 @@
+// https://stackoverflow.com/questions/40162907/w3includehtml-sometimes-includes-twice
+/*
+function xLuIncludeFile() {
+    let z, i, a, file, xhttp;
+
+    z = document.getElementsByTagName("*");
+
+    for (i = 0; i < z.length; i++) {
+        if (z[i].getAttribute("xlu-include-file")) {
+            a = z[i].cloneNode(false);
+            file = z[i].getAttribute("xlu-include-file");
+            xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    a.removeAttribute("xlu-include-file");
+                    a.innerHTML = xhttp.responseText;
+                    z[i].parentNode.replaceChild(a, z[i]);
+                    xLuIncludeFile();
+                }
+            }
+
+            // false makes the send operation synchronous, which solves a problem
+            // when using this function in short pages with Chrome. But it is
+            // deprecated on the main thread due to its impact on responsiveness.
+            // This call may end up throwing an exception someday.
+
+            xhttp.open("GET", file, false);
+            xhttp.send();
+
+            return;
+        }
+    }
+}
+*/
+
+async function xLuIncludeFile() {
+    let z = document.getElementsByTagName("*");
+
+    for (let i = 0; i < z.length; i++) {
+        if (z[i].getAttribute("xlu-include-file")) {
+            let a = z[i].cloneNode(false);
+            let file = z[i].getAttribute("xlu-include-file");
+
+            try {
+                let response = await fetch(file);
+                if (response.ok) {
+
+                    let content = await response.text();
+
+                    if (file === "catalogoRecuadro.html") {
+                        let articleData = {
+                            nombre: z[i].getAttribute("data-nombre") || 'Sin nombre',
+                            especie: z[i].getAttribute("data-especie") || 'Desconocida',
+                            edad: z[i].getAttribute("data-edad") || 'Desconocida'
+                        };
+
+                        content = content.replace(/{{nombre}}/g, articleData.nombre)
+                                        .replace(/{{especie}}/g, articleData.especie)
+                                        .replace(/{{edad}}/g, articleData.edad);
+                    }
+
+
+                    a.removeAttribute("xlu-include-file");
+                    //a.innerHTML = await response.text();
+                    a.innerHTML = content;
+                    z[i].parentNode.replaceChild(a, z[i]);
+                    xLuIncludeFile();
+                }
+            } catch (error) {
+                console.error("Error fetching file:", error);
+            }
+
+            return;
+        }
+    }
+}
+
