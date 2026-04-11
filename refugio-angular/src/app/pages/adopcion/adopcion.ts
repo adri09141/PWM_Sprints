d2f5d1f16d544699f6656e9920d1e0ccced89f22
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-04-11 14:05:11 +0100

    Manejo del formulario con angular en /Adopcion

--- refugio-angular/src/app/pages/adopcion/adopcion.ts
+++ refugio-angular/src/app/pages/adopcion/adopcion.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-adopcion',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './adopcion.html',
   styleUrl: './adopcion.css',
 })
-export class Adopcion {}
+export class Adopcion {
+  constructor(private router: Router) {}
+  botonPulsado: boolean = false;
+  home(form: NgForm) {
+    if(form.valid) {
+      this.router.navigate(['/']);
+      alert("Solicitud enviada, pendiente de validar");
+    } else {
+      console.log("El formulario no es válido aún");
+    }
+  }
+
+  protected readonly name = name;
+}
