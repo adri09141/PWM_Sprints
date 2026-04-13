bbdf5d3ab574512f8caa39cf6af50d55381dfe3d
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-04-13 16:43:33 +0100

    Validacion de AñadirReseña, falta que cargue algunos datos

--- refugio-angular/src/app/pages/anadir-resena/anadir-resena.ts
+++ refugio-angular/src/app/pages/anadir-resena/anadir-resena.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-anadir-resena',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
   templateUrl: './anadir-resena.html',
   styleUrl: './anadir-resena.css',
 })
-export class AnadirResena {}
+export class AnadirResena {
+  constructor(private router: Router) {}
+  archivoFoto: File | null = null;
+  fotoCancelada: boolean = false;
+  enviarDatos(formulario: NgForm)
+  {
+    if (formulario.invalid) {
+      return;
+    }
+    console.log(formulario.value)
+    alert("Estamos validando tu reseña sobre tu pequeño: " + formulario.value.nombre_mascota);
+    this.router.navigate(['/']);
+  }
+  capturarFoto(event: any) {
+    const archivoQueHaSubido = event.target.files[0];
+    if (archivoQueHaSubido) {
+      this.archivoFoto = archivoQueHaSubido;
+      this.fotoCancelada = false; // Si sube foto, apagamos el error
+    }
+  }
+}
