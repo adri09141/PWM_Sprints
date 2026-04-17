def710e40df8380edab77797187b04ba7155070e
adri091410 <141488778+adri09141@users.noreply.github.com>
2026-04-17 23:20:00 +0100

    Funcion de crearCuenta, falta eliminar las otras cuentas del json y crear la funcion de iniciarSesion

--- refugio-angular/src/app/pages/crear-cuenta/crear-cuenta.ts
+++ refugio-angular/src/app/pages/crear-cuenta/crear-cuenta.ts
@@ -1,6 +1,7 @@
 import {booleanAttribute, Component} from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {FormsModule, NgModel} from '@angular/forms';
+import {FormsModule, NgModel, NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 import { DatabaseService } from '../../services/database';
 @Component({
   selector: 'app-crear-cuenta',
@@ -12,7 +13,7 @@ import { DatabaseService } from '../../services/database';
 export class CrearCuenta {
   menorEdad: boolean = true;
   verContrasena: boolean = false;
-  constructor(private db: DatabaseService) {} // Inyectamos tu servicio
+  constructor(private db: DatabaseService, private router: Router) {} // Inyectamos tu servicio
   toggleVerContrasena() {
     this.verContrasena = !this.verContrasena;
   }
@@ -21,7 +22,6 @@ export class CrearCuenta {
     const hoy = new Date();
     const fechaNacimiento = new Date(fecha.value);
     const edadMinima = new Date();
-
     edadMinima.setFullYear(hoy.getFullYear() - 18);
 
     if (fechaNacimiento < edadMinima) {
@@ -29,6 +29,24 @@ export class CrearCuenta {
     } else {
       this.menorEdad = true;
     }
+
+  }
+  crearCuenta(formulario: NgForm)
+  {
+    const datos = formulario.value;
+    datos.numeroAdopciones = 0;
+    this.db.insertar("usuarios", datos) // Te lo he cambiado a "usuarios" para que no haya mil carpetas raras
+      .then(() => {
+        // 💬 AVISO: Si ha ido bien, celebramos y limpiamos
+        alert("🎉 ¡Bienvenido a la familia! Tu cuenta ha sido creada.");
+        formulario.resetForm(); // Esto vacía todos los inputs automáticamente
+        this.router.navigate(['/']);
+      })
+      .catch((error) => {
+        // Por si acaso se cae internet o falla algo
+        console.error("Error al crear cuenta:", error);
+        alert("❌ Vaya, hubo un problema al conectar con el servidor.");
+      });
 
   }
 }
